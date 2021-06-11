5bdf812a8541ea1db8699f9a840c7ca35c14fff2
bernard-david <dpbernard18@gmail.com>
2021-06-11 18:55:39 -0400

    rendering input data into the template

--- server.py
+++ server.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 def show_form():
     return render_template("index.html")
 
+@ app.route('/result', methods=['POST'])
+def result():
+    request.form['name']
+    location_from_form = request.form['location']
+    language_from_form = request.form['language']
+    comment_from_form = request.form['comment']
+    return render_template("result.html", name_on_template=request.form['name'], language_on_template=request.form['location'], location_on_template=request.form['location'], comment_on_template=comment_from_form,)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
