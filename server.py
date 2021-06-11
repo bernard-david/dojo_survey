0a8d255cf949f27a42a9da1510241b5faa0665de
bernard-david <dpbernard18@gmail.com>
2021-06-10 22:36:29 -0400

    current styling of form

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
-
+@app.route('/')
+def show_form():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
